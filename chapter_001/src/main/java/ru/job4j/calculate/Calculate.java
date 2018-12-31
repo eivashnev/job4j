ab653fcd321c1c9a0c2462ad3a52d3497458e518
eivashnev <zheniog@yandex.ru>
2019-01-01 00:27:23 +0300

    1.3. JavaDoc. Code convention [#4087]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,6 +1,15 @@
 package ru.job4j.calculate;
 
+/**
+ * Calculate.
+ *
+ * @author Evgeny Ivashnev (zheniog@yandex.ru)
+ */
 public class Calculate{
+		/**
+		* Main.
+		* @param args - arguments.
+		*/
 		public static void main(String[] args){
 			System.out.println("Hello world!");
 		}
