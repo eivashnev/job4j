3a31f240885b6daed760e6639c6b278be5609306
eivashnev <zheniog@yandex.ru>
2019-01-01 18:27:54 +0300

    Fix: 3.1. Элементарный калькулятор. [#185] - добавлены JavaDocs

--- chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
+++ chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
@@ -1,25 +1,59 @@
 package ru.job4j.calculator;
 
+/**
+ * Calculator program.
+ *
+ * @author Evgeny Ivashnev (zheniog@yandex.ru)
+ */
 public class Calculator {
     private double result;
 
+    /**
+     * Add operation.
+     *
+     * @param first
+     * @param second
+     */
     public void add(double first, double second) {
         this.result = first + second;
     }
 
+    /**
+     * Subtract operation.
+     *
+     * @param first
+     * @param second
+     */
     public void subtract(double first, double second) {
         this.result = second - first;
     }
 
-    public double getResult() {
-        return this.result;
-    }
-
+    /**
+     * Multiply operation.
+     *
+     * @param first
+     * @param second
+     */
     public void multiply(double first, double second) {
         this.result = first * second;
     }
 
+    /**
+     * Divide operation.
+     *
+     * @param first
+     * @param second
+     */
     public void divide(double first, double second) {
         this.result = first / second;
+    }
+
+    /**
+     * Method returns last operation result.
+     *
+     * @return result
+     */
+    public double getResult() {
+        return this.result;
     }
 }
