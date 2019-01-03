29a87b6fbcb039f0422a194f288ee71fdd4d4680
eivashnev <zheniog@yandex.ru>
2019-01-03 21:24:08 +0300

    Fix: 6.5. Создать программу для сортировки массива методом перестановки[#102058] - optimized

--- chapter_001/src/main/java/ru/job4j/array/BubbleSort.java
+++ chapter_001/src/main/java/ru/job4j/array/BubbleSort.java
@@ -1,7 +1,9 @@
 package ru.job4j.array;
 
+import java.util.Arrays;
+
 /**
- * Bubblesort.
+ * Bubble sort.
  *
  * @author - Evgeny Ivashnev (zheniog@yandex.ru)
  */
@@ -12,12 +14,12 @@ public class BubbleSort {
      * @return sorted array
      */
     public int[] sort(int[] array) {
-        for (int j : array) {
-            for (int i = 0; i < array.length - 1; i++) {
-                if (array[i] > array[i + 1]) {
-                    int tmp = array[i];
-                    array[i] = array[i + 1];
-                    array[i + 1] = tmp;
+        for( int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array.length - 1 - i; j++) {
+                if (array[j] > array[j + 1]) {
+                    int tmp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = tmp;
                 }
             }
         }
--- chapter_001/src/test/java/ru/job4j/array/BubbleSortTest.java
+++ chapter_001/src/test/java/ru/job4j/array/BubbleSortTest.java
@@ -8,7 +8,7 @@ import static org.junit.Assert.*;
 public class BubbleSortTest {
     @Test
     public void whenSortArrayWithTenElementsThenSortedArray() {
-        int[] array     = new int[] {9, 1, 2, 5, 6, 3, 0, 4, 8, 7};
+        int[] array     = new int[] {3, 8, 1, 6, 4, 5, 2, 7, 9, 0};
         int[] expected  = new int[] {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
         int[] result = new BubbleSort().sort(array);
         assertThat(result, is(expected));
