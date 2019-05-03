8558a6b96cb03c1757e3dfa4b7738f083b445fa2
eivashnev <zheniog@yandex.ru>
2019-05-03 15:39:07 +0300

    3. Уникальность элементов и сортировка. [#110225] - Fix sort before distinct

--- chapter_004/src/main/java/ru/job4j/streams/turfirma/Profiles.java
+++ chapter_004/src/main/java/ru/job4j/streams/turfirma/Profiles.java
@@ -8,8 +8,8 @@ import java.util.stream.Collectors;
 public class Profiles {
     List<Address> collect(List<Profile> profiles) {
         return profiles.stream().map(profile -> profile.getAddress())
-                .distinct()
                 .sorted(Comparator.comparing(Address::getCity))
+                .distinct()
                 .collect(Collectors.toList());
     }
 
